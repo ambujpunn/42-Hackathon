c4046718728d94b946690cf19a0ce8090e115cf8
Ambuj Punn <ambujpunn@gmail.com>
2018-04-28 22:46:52 -0700

    weather api added

--- Safe Adventures/ViewController.swift
+++ Safe Adventures/ViewController.swift
@@ -47,6 +47,30 @@ struct HikingGear: Decodable {
     }
 }
 
+struct Weather {
+    var today: Bool
+    var currentTemp: Float?
+    var dayTempMax: Float?
+    var dayTempMaxTime: Date?
+    var dayTempMin: Float?
+    var dayTempMinTime: Date?
+    var summary: String?
+    var iconInfo: Icon?
+    var alert: String?
+    
+    init(today: Bool, foreCast: Forecast) {
+        self.today = today
+        self.currentTemp = foreCast.currently?.apparentTemperature
+        self.dayTempMax = foreCast.daily?.data.first?.apparentTemperatureMax
+        self.dayTempMin = foreCast.daily?.data.first?.apparentTemperatureMin
+        self.dayTempMaxTime = foreCast.daily?.data.first?.apparentTemperatureMaxTime
+        self.dayTempMinTime = foreCast.daily?.data.first?.apparentTemperatureMinTime
+        self.summary = foreCast.daily?.data.first?.summary
+        self.iconInfo = foreCast.daily?.data.first?.icon
+        self.alert = foreCast.alerts?.first?.description
+    }
+}
+
 class ViewController: UIViewController {
     private let darkSkyAPIKey = "4a76b0aa817f065da1ac4e81d94b70de"
     let weatherClient: DarkSkyClient
@@ -60,10 +84,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Testing
         loadPark("yosemite") { (park) in
             if park != nil {
                 print(park!.description)
                 let (lat, long) = parseLatLongString(latLongString: park!.location)
+                weatherToday(lat: lat, long: long, completionHandler: { (forecast) in
+                    let weatherDataModel = Weather(today: true, foreCast: forecast)
+                    print("Today: \n")
+                    print(weatherDataModel)
+                })
+                let testDate = Date(timeIntervalSince1970: 1525003200)
+                weatherDate(date: testDate, lat: lat, long: long, completionHandler: { (forecast) in
+                    let weatherDataModel = Weather(today: false, foreCast: forecast)
+                    print("later: \n")
+                    print(weatherDataModel)
+                })
             }
         }
         
@@ -75,13 +111,39 @@ class ViewController: UIViewController {
     }
     
     // Private - Call DarkSky APIs
-    /*
-    func weather(_ today: Bool, ) -> <#return type#> {
-        <#function body#>
-    }*/
+    // Pass in completion handler
+    func weatherToday(lat: String, long: String, completionHandler: @escaping (Forecast) -> Void) {
+        // Force unwrapping because we are sure the lat long will be legit
+        weatherClient.getForecast(latitude: Double(lat)!, longitude: Double(long)!, extendHourly: false, excludeFields: [.hourly, .minutely, .flags]) { (result) in
+            switch result {
+            case .success(let currentForecast, let requestMetadata):
+                completionHandler(currentForecast)
+            case .failure(let error):
+                print (error)
+            }
+        }
+        
+        //Note: Use currently's temperature to get current temperature
+        // Use daily's data's first element to get current day's temp max/min and temp max/min time
+    }
 
+    func weatherDate(date: Date, lat: String, long: String, completionHandler: @escaping (Forecast) -> Void) {
+        // Force unwrapping because we are sure the lat long will be legit
+        weatherClient.getForecast(latitude: Double(lat)!, longitude: Double(long)!, time: date, excludeFields: [.hourly, .minutely, .flags]) { (result) in
+            switch result {
+            case .success(let currentForecast, let requestMetadata):
+                completionHandler(currentForecast)
+            case .failure(let error):
+                print (error)
+            }
+        }
+        
+        //Note: Use currently's temperature to get current temperature
+        // Use daily's data's first element to get current day's temp max/min and temp max/min time
+    }
+    
     // Private - Load Mock JSON
-    // Lots of reusable code, but not sure how to take in generic version of Decodable, look more into this
+    // Lots of reusable code, but not sure how to take in generic version opf Decodable, look more into this
     // Reference: https://stackoverflow.com/questions/46962021/how-to-reference-a-generic-decodable-struct-in-swift-4
     
     private func loadPark(_ name: String, completionHandler: (Park?) -> Void) {
