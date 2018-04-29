6d85b65a31c9fc75514b9751153668bbf9f270d9
Ambuj Punn <ambujpunn@gmail.com>
2018-04-28 19:19:31 -0700

    skeleton for park/gear methods

---
+++ Safe Adventures/API.swift
@@ -0,0 +1,25 @@
+//
+//  API.swift
+//  Safe Adventures
+//
+//  Created by Ambuj Punn on 4/28/18.
+//  Copyright © 2018 Ambuj Punn. All rights reserved.
+//
+
+import Foundation
+import ForecastIO
+// class not really needed
+class API {
+    static let sharedClient = API()
+    
+    private let darkSkyAPIKey = "4a76b0aa817f065da1ac4e81d94b70de"
+    //private let weatherAPI = DarkSkyClient(apiKey: darkSkyAPIKey)
+    
+    private init() {
+        
+    }
+    /*
+    func weatherToday(lat: Float, long: Float) -> <#return type#> {
+        <#function body#>
+    }*/
+}
--- Safe Adventures/ViewController.swift
+++ Safe Adventures/ViewController.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
+struct Park: Decodable {
+    var name: String
+    var location: String
+    var description: String
+    var url: String
+    var imageUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "fullName"
+        case location = "latLong"
+        
+        case description
+        case imageUrl
+        case url
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -20,6 +37,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Private
+    
+    private func loadPark(_ name: String, completionHandler: (Park) -> Void) {
+        if let parkJsonData = loadJson("parks") {
+            do {
+                let park = try JSONDecoder().decode(Park.self, from: parkJsonData)
+                completionHandler(park)
+            } catch {
+                print("error:\(error)")
+            }
+        }
+    }
+    
+    private func loadHikingGear(_ name: String, completionHandler: (Park) -> Void) {
+        if let parkJsonData = loadJson("parks") {
+            do {
+                let park = try JSONDecoder().decode(Park.self, from: parkJsonData)
+                completionHandler(park)
+            } catch {
+                print("error:\(error)")
+            }
+        }
+    }
+    
+    private func loadJson(_ fileName: String) -> Data? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                return data
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
+    
 }
 
