1d1b691f4f6f661e2c0b39278c29f3f196d85325
Ambuj Punn <ambujpunn@gmail.com>
2018-04-28 20:00:40 -0700

    park able to get data

--- Safe Adventures/ViewController.swift
+++ Safe Adventures/ViewController.swift
@@ -7,6 +7,15 @@
 //
 
 import UIKit
+import ForecastIO
+
+struct Parks: Decodable {
+    var parkList: [Park]
+    
+    enum CodingKeys: String, CodingKey {
+        case parkList = "parks"
+    }
+}
 
 struct Park: Decodable {
     var name: String
@@ -39,29 +48,71 @@ struct HikingGear: Decodable {
 }
 
 class ViewController: UIViewController {
-
+    private let darkSkyAPIKey = "4a76b0aa817f065da1ac4e81d94b70de"
+    let weatherClient: DarkSkyClient
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        weatherClient = DarkSkyClient(apiKey: darkSkyAPIKey)
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadPark("yosemite") { (park) in
+            if park != nil {
+                print(park!.description)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // Private
     
-    private func loadPark(_ name: String, completionHandler: (Park) -> Void) {
+    // Private - Call DarkSky APIs
+    /*
+    func weather(_ today: Bool, ) -> <#return type#> {
+        <#function body#>
+    }*/
+
+    // Private - Load Mock JSON
+    // Lots of reusable code, but not sure how to take in generic version of Decodable, look more into this
+    // Reference: https://stackoverflow.com/questions/46962021/how-to-reference-a-generic-decodable-struct-in-swift-4
+    
+    private func loadPark(_ name: String, completionHandler: (Park?) -> Void) {
         if let parkJsonData = loadJson("parks") {
             do {
-                let park = try JSONDecoder().decode(Park.self, from: parkJsonData)
-                completionHandler(park)
+                let parks = try JSONDecoder().decode(Parks.self, from: parkJsonData)
+                // Only returns data once the entire name of the park is entered (it's a hackathon remember, it's okay to not write great code lol
+                // Filter it so the match is the first element
+                let foundPark = parks.parkList.filter({ (park) -> Bool in
+                    return park.name.lowercased().contains(name)
+                }).first
+                completionHandler(foundPark)
             } catch {
                 print("error:\(error)")
             }
         }
     }
+    
+    /*
+    private func parseLatLongString(latLongString: String) -> (lat: String, long: String) {
+        // Format is: "lat:37.29839254, long:-113.0265138"
+        let latLongSplitArray = latLongString.split(separator: ",")
+        let lat = latLongSplitArray.first
+        let long = latLongSplitArray.last
+        
+        // Format is: "lat:37.29839254" and "long:-113.0265138"
+        if let
+    }*/
     
     private func loadHikingGear(_ name: String, completionHandler: (HikingGear) -> Void) {
         if let hikingGearJsonData = loadJson("hiking-gear") {
